dbc98984f9c8cf35ef8b8cdca9ace28a3d2a064d
shuhasegawa <shuhasegawa0430@gmail.com>
2023-09-18 19:37:23 +0900

    リファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,36 @@ import { v4 } from 'uuid';
 
 function App() {
   const [colors, setColors] = useState(colorData);
+
+  const add = (title: string, color: string) => {
+    const newColor = [
+      ...colors,
+      {
+        id: v4() as string,
+        rating: 0,
+        title,
+        color,
+      },
+    ];
+    setColors(newColor);
+  };
+
+  const setRate = (id: string, rating: number) => {
+    const newColors = colors.map((color) =>
+      color.id === id ? { ...color, rating } : color
+    );
+    setColors(newColors);
+  };
+
+  const remove = (id: string) => {
+    const newColors = colors.filter((color) => color.id !== id);
+    setColors(newColors);
+  };
+
   return (
     <>
-      <AddColorForm
-        onNewColor={(title, color) => {
-          const newColor = [
-            ...colors,
-            {
-              id: v4() as string,
-              rating: 0,
-              title,
-              color,
-            },
-          ];
-          setColors(newColor);
-        }}
-      />
-      <ColorList
-        colors={colors}
-        onRateColor={(id, rating) => {
-          const newColors = colors.map((color) =>
-            color.id === id ? { ...color, rating } : color
-          );
-          setColors(newColors);
-        }}
-        onRemoveColor={(id) => {
-          const newColor = colors.filter((color) => color.id !== id);
-          setColors(newColor);
-        }}
-      />
+      <AddColorForm onNewColor={add} />
+      <ColorList colors={colors} onRateColor={setRate} onRemoveColor={remove} />
     </>
   );
 }
